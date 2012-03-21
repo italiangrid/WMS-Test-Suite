64f86e33b76f8f1340be773a41b31f3b88332a0f
Aristotelis Kretsis <akretsis@ceid.upatras.gr>
2012-03-21 11:06:46 +0200

    ensure that logged failed reason is 'Prologue failed with error 1'

--- regression_tests/bugs/75223.py
+++ regression_tests/bugs/75223.py
@@ -24,18 +24,31 @@ def run(utils):
         utils.use_utils_jdl()
         utils.set_jdl(utils.get_jdl_file())
         utils.set_destination_ce(utils.get_jdl_name(), dest)
-
+        utils.add_jdl_attribute(utils.get_jdl_file(),"Prologue","/bin/false")
 
         JOBID=Job_utils.submit_wait_finish(utils,dest)
 
-        logging.info("Check failing reasons")
+        logging.info("Check if failed reason is 'Prologue failed with error 1'")
+    
+        result=utils.run_command_continue_on_error("glite-wms-job-logging-info -v 2 --event Done %s"%(JOBID))
+ 
+        blocks=result.split('Event: Done')
+
+        logmonitor_reasons=[]
+
+        for block in blocks:
+          if block.find('LogMonitor')!=-1:
+             lines=block.split('\n')
+             for line in lines:
+                if line.find('Reason')!=-1:
+                  logmonitor_reasons.append(line.split(' = ')[1].strip(' \t\n'))
+                  break
+
         
-        reason=utils.getLoggedReason(JOBID)
-        
-        for msg in reason:
-            if msg.find("LM_log_done_begin") != -1:
-                logging.error("Wrong message found in %s",msg)
-                raise GeneralError("Check failig reasons","Wrong message found in %s"%(msg))
+        for reason in logmonitor_reasons:
+            if reason!='Prologue failed with error 1' and reason!='prologue failed with error 1':
+                logging.error("Wrong faild reason for LogMonitor. Found '%s' , while expected is 'Prologue failed with error 1'",reason)
+                raise GeneralError("Check failend reasons for LogMonitor","Wrong failed reason. Found %s , while expected is 'Prologue failed with error 1'"%(reason))
                 break
-     
+        
     logging.info("End of regression test for bug %s"%(bug))
