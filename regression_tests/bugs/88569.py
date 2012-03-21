421b4f1671f82678d9e7d66732a268b4bc57cece
Aristotelis Kretsis <akretsis@ceid.upatras.gr>
2012-03-21 11:04:38 +0200

    changed to improve the way test looking for logged lines in lcmaps.log

--- regression_tests/bugs/88569.py
+++ regression_tests/bugs/88569.py
@@ -26,6 +26,16 @@ def run(utils):
 
     utils.set_jdl(utils.get_jdl_file())
 
+    log_file="/var/log/glite/lcmaps.log"
+
+    logging.info("Check if dedicated log file %s exists"%(log_file))
+
+    utils.execute_remote_cmd(ssh,"ls -l %s"%(log_file))
+
+    logging.info("Compute number of lines in log file %s before job submission"%(log_file))
+
+    log_file_lines_before=utils.execute_remote_cmd(ssh,"grep LCMAPS %s | wc -l "%(log_file))
+
     logging.info("Submit a job")
 
     JOBID=utils.run_command_continue_on_error("glite-wms-job-submit %s --config %s --nomsg %s"%(utils.get_delegation_options(),utils.get_config_file(),utils.get_jdl_file()))
@@ -34,7 +44,7 @@ def run(utils):
 
     utils.run_command_continue_on_error("glite-wms-job-cancel --noint %s"%(JOBID))
 
-    logging.info("Check that no messages about LCMPAS is logged by the wmproxy on syslog during a submission")
+    logging.info("Check that no messages about LCMPAS is logged by the wmproxy on syslog during submission")
 
     files=utils.execute_remote_cmd(ssh,"ls /var/log/messages*").split(" ")
 
@@ -50,14 +60,16 @@ def run(utils):
     if output[0]==0: #return code 0 means find messages in syslog
        logging.error("Find LCMAPS messages by the wmproxy on syslog")
        raise GeneralError("Check syslog for LCMAPS messages by the wmproxy","Find LCMAPS messages by the wmproxy on syslog")
+    
+    logging.info("Check if messages about LCMAPS are saved in dedicated log file: %s"%(log_file))
 
-    log_file="/var/log/glite/lcmaps.log"
+    log_file_lines_after=utils.execute_remote_cmd(ssh,"grep LCMAPS %s | wc -l "%(log_file))
 
-    logging.info("Check if messages about LCMAPS are saved in a dedicated log file: %s"%(log_file))
-
-    output=utils.execute_remote_cmd(ssh,"ls -l %s"%(log_file))
-
-    utils.close_ssh(ssh)
+    if int(log_file_lines_after) > int(log_file_lines_before) :
+       logging.info("Check OK. Find LCMAPS messages in dedicated log file: %s"%(log_file))
+    else:
+       logging.error("Unable to find LCMAPS messages in dedicated log file: %s"%(log_file))
+       raise GeneralError("Check dedicated log file %s for LCMAPS messages"%(log_file),"Unable to find LCMAPS messages in dedicated log file:%s"%(log_file))
 
     logging.info("Test OK")
 
